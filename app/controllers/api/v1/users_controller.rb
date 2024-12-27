d2bffe77c20f41ca09022e99a63686aa9cf204d3
Safi Ullah <dev.safi.96@gmail.com>
2024-12-27 11:41:21 +0500

    added image attributes for users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,8 @@ class Api::V1::UsersController < ApplicationController
       private
     
       def user_params
-        params.require(:user).permit(:email, :password, :name, :role, :tenant_id)
+        params.require(:user).permit(:email, :password, :name, :role, :tenant_id, 
+          :profile_picture, # For Active Storage
+          image_attributes: [:id, :file_name, :byte_size, :file_type, :_destroy]) # Nested attributes
       end
 end
